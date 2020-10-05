def76992312d5725cc32a0718df6e1e8dea7081d
Crystal Knight <crystalknightcodes@gmail.com>
2020-10-05 16:07:12 -0400

    Updated User model and networking to include "role" property.

---
+++ labs-ios-starter/Models/Enums/Role.swift
@@ -0,0 +1,15 @@
+//
+//  Role.swift
+//  labs-ios-starter
+//
+//  Created by Christy Hicks on 10/5/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+enum Role: String {
+    case HOTEL
+    case HUB_ADMIN
+    case HUB_USER
+}
--- labs-ios-starter/Networking/Queries.swift
+++ labs-ios-starter/Networking/Queries.swift
@@ -198,6 +198,7 @@ class Queries: Request {
         phone
         skype
         signupTime
+        role
         properties {
             id
         }
