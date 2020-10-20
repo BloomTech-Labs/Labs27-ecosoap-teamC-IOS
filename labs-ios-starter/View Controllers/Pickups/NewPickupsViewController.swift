fb219db5b7b23a7efa32980d0bcc168f6c5b8d1a
bharatk2 <bharkumar8@gmail.com>
2020-10-20 15:35:40 -0700

    Created view for pickups

--- labs-ios-starter/View Controllers/Pickups/NewPickupsViewController.swift
+++ labs-ios-starter/View Controllers/Pickups/NewPickupsViewController.swift
@@ -10,12 +10,45 @@ import UIKit
 
 class NewPickupsViewController: UIViewController {
 
+    @IBOutlet weak var newPickTableView: UITableView!
+    @IBOutlet weak var oldPickTableView: UITableView!
+    
+    var controller = BackendController.shared
+    
+    private var pickUp: Pickup? {
+        didSet {
+            updateViews()
+        }
+    }
+    private var pickUpData: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
+        newPickTableView.delegate = self
+        newPickTableView.dataSource = self
+        
+        oldPickTableView.delegate = self
+        oldPickTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    private func updateViews() {
+        guard let pickUp = pickUp else { return }
+        for property in controller.properties.values {
+            controller.pickupsByPropertyId(propertyId: property.id) { error in
+                if let error = error {
+                    NSLog("Error in getting the pickups dates: \(error)")
+                    return
+                }
+                DispatchQueue.main.async {
+                    print("pickups fetched succesfully")
+                    self.newPickTableView.reloadData()
+                    self.oldPickTableView.reloadData()
+                }
+            }
+        }
+        pickUpData.append(pickUp.pickupDate?.asShortDateString() ?? "")
+    }
 
     /*
     // MARK: - Navigation
@@ -28,3 +61,19 @@ class NewPickupsViewController: UIViewController {
     */
 
 }
+
+extension NewPickupsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pickUpData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == newPickTableView,
+            let cell = tableView.dequeueReusableCell(withIdentifier: "newPickupCell") {
+            cell.textLabel?.text = pickUpData[indexPath.row]
+            return cell
+        }
+            return UITableViewCell()
+        }
+    }
+
